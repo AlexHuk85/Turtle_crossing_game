1f746e557f83ef96e00dbee09b2df1911b726625
alex <alex.huk85@gmail.com>
2021-01-04 05:51:16 +1100

    Genarate car randomly Done

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,27 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape('square')
+        self.random_color = random.choice(COLORS)
+        self.color(self.random_color)
+        self.shapesize(1, 2)
+        self.penup()
+        self.setheading(180)
+        self.random_number = random.randint(-250, 270)
+        self.goto(300, self.random_number)
+        self.move_forward()
+
+
+    def move_forward(self):
+        if self.xcor() > -280:
+            new_speed = self.xcor() - STARTING_MOVE_DISTANCE
+            self.goto(new_speed, self.ycor())
--- main.py
+++ main.py
@@ -3,14 +3,18 @@ from turtle import Screen
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
+import random
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 screen.listen()
+car_list = []
 
 player = Player()
 scoreboard = Scoreboard()
+
+car_list = []
 
 screen.onkey(player.move_forward, 'Up')
 
@@ -18,6 +22,12 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car = CarManager()
+    car_list.append(car)
+    for c in car_list:
+        c.move_forward()
+
 
     # Update score board if player cross the finish line
     if player.ycor() > 280:
