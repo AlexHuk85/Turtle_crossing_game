687ae3d98d71bc06b8d6c71fb7c6ad08db0cad82
alex <alex.huk85@gmail.com>
2021-01-04 19:30:33 +1100

    Car control

--- car_manager.py
+++ car_manager.py
@@ -10,18 +10,21 @@ MOVE_INCREMENT = 10
 class CarManager(Turtle):
     def __init__(self):
         super().__init__()
-        self.shape('square')
-        self.random_color = random.choice(COLORS)
-        self.color(self.random_color)
-        self.shapesize(1, 2)
-        self.penup()
-        self.setheading(180)
-        self.random_number = random.randint(-250, 270)
-        self.goto(300, self.random_number)
-        self.move_forward()
+        random_car = random.randint(1, 6)
+        if random_car == 1:
+            self.shape('square')
+            self.random_color = random.choice(COLORS)
+            self.color(self.random_color)
+            self.shapesize(1, 2)
+            self.penup()
+            self.setheading(180)
+            self.random_number = random.randint(-250, 270)
+            self.goto(300, self.random_number)
+        else:
+            self.hideturtle()
+            self.penup()
 
 
     def move_forward(self):
-        if self.xcor() > -280:
-            new_speed = self.xcor() - STARTING_MOVE_DISTANCE
-            self.goto(new_speed, self.ycor())
+        new_speed = self.xcor() - STARTING_MOVE_DISTANCE
+        self.goto(new_speed, self.ycor())
